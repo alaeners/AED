94348a72e84346a2a5424b0f8488be18d433c230
alaeners <alaenedesousa@gmail.com>
2018-10-21 17:45:58 -0300

    TI AED COM SO --start

--- TI_with_SO/TI_with_SO/Program.cs
+++ TI_with_SO/TI_with_SO/Program.cs
@@ -10,6 +10,20 @@ namespace TI_with_SO
     {
         static void Main(string[] args)
         {
+            Processo processo;
+
+            StreamReader rd = new StreamReader(ofd.FileName, true);
+            string[] dados;
+            string linha;
+
+            while (!rd.EndOfStream)
+            {
+                linha = rd.ReadLine();
+                dados = linha.Split(';');
+                processo = new Processo(int.Parse(dados[0]), dados[1], int.Parse(dados[2]), float.Parse(dados[3]), int.Parse(dados[4]));
+                ListaProcessos.Add(processo);
+            }
+
         }
     }
 }
